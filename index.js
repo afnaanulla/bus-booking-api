85f690028d268e58eaa7eff28ef7b04bdfbb4fae
MohammadAfnaan <afnaan31250@gmail.com>
2025-09-03 00:52:06 +0530

    Updated

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const multer = require("multer");
 
 const app = express();
-app.use(cors({origin: 'https://bus-booking-api-f-fu2p.vercel.app/',}));
+app.use(cors());
 app.use(express.json());
 
 const upload = multer({ storage: multer.memoryStorage() });
